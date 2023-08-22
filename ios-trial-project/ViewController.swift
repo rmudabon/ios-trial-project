aa6c67ea5d24b3aabf6d3679d9885e9866977cbd
Rowmel Marco Dabon <rmdabs@gmail.com>
2023-08-22 18:05:49 +0800

    Initial Commit

---
+++ ios-trial-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-trial-project
+//
+//  Created by Rowmel Marco Dabon on 8/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
